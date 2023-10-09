63b967bc11ef8684761462163aa31d7047181c7a
Allen Lunsford <alunsfor@gs690-prohawk.local>
2023-10-08 22:44:42 -0400

    Languages for the item label

--- pages/tab-description.js
+++ pages/tab-description.js
@@ -1,10 +1,14 @@
 let tabs = document.querySelectorAll(".tab-description-label h3");
 let tabDescription = document.querySelectorAll(".tab-description-content div");
+let tabLabel = document.querySelectorAll(".tab-label-content div");
 let tabMetadata = document.querySelectorAll(".tab-metadata-content div");
 
 tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
     tabDescription.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabLabel.forEach((content) => {
       content.classList.remove("active");
     });
     tabMetadata.forEach((content) => {
@@ -14,6 +18,7 @@ tabs.forEach((tab, index) => {
       tab.classList.remove("active");
     });
     tabDescription[index].classList.add("active");
+    tabLabel[index].classList.add("active");
     tabMetadata[index].classList.add("active");
     tabs[index].classList.add("active");
     localStorage.setItem('ura_lang_index', JSON.stringify(index));
@@ -25,6 +30,7 @@ var index_def = 0;
 if (index_def_string  !== null) {
   index_def = JSON.parse(index_def_string); }
 tabDescription[index_def].classList.add("active");
+tabLabel[index_def].classList.add("active");
 tabMetadata[index_def].classList.add("active");
 tabs[index_def].classList.add("active");
 
